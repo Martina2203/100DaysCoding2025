c91869d6d7f93d89532dce2bccb4903608e0fd55
Martina2203 <martinatina482@gmail.com>
2025-09-09 20:57:09 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,17 @@
+package day2;
+
+public class Day2 {
+
+  public static void main (String[] args){
+
+      String Nama = "Martina";
+      int Bb = 35;
+      double Tb = 142.5;
+
+      System.out.println("Nama:"+Nama);
+      System.out.print("Bb:"+Bb);
+      System.out.printf("\nTb:%f\n", Tb);
+
+  }
+
+}
