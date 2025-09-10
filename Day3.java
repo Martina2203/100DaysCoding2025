e773de1a3da866ce7854cc401ef09ca3a566410a
Martina2203 <martinatina482@gmail.com>
2025-09-10 19:38:53 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,22 @@
+package day3;
+
+public class Day3 {
+
+   public static void main (String[] args) { 
+
+       /* Program ini berisi data sederhana tentang saya
+          untuk contoh komentar menggunakan tanda bintang dan garis miring */
+
+   String nama = "Martina"; // menyimpan nama
+   int umur = 18; // menyimpan umur
+
+
+     // Menampilkan hasil ke layar
+     System.out.println("nama: " + nama);
+     System.out.println("umur: " + umur + "tahun");
+
+
+   }
+
+}
+  
