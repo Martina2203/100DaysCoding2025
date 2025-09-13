3cc2328bb8cbfff410409d274f6dd9a4d96f4244
Martina2203 <martinatina482@gmail.com>
2025-09-13 12:22:30 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,24 @@
+package day6;
+
+public class Day6 {
+    
+    public static void main(String[] args) {
+        // Ukuran 4 byte (32 bit) 
+        float berat = 33.2f;
+        
+        // Ukuran 8 byte (64 bit)
+        double tinggi = 144.2;
+      
+        
+        // Menampilkan hasil output
+        System.out.println("Berat badan saya yaitu " + berat + "kg");
+        System.out.println("Tinggi badan saya sekitar " + tinggi + "cm");
+        
+        /* 
+         Perbedaan dari float dan double yaitu ukuran penyimpanan double lebih besar dari pada
+         float dan juga kita harus menambahkan huruf f dibelakang angka float contoh 33.2f
+        */
+        
+    }
+    
+}
