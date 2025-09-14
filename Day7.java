a3332d4236bf1651557ca041e8ca4c45c69b49fe
Martina2203 <martinatina482@gmail.com>
2025-09-14 11:03:37 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,24 @@
+package day7;
+
+public class Day7 {
+
+    public static void main(String[] args) {
+        // Untuk menyimpan 1 karakter tunggal
+        char nilai = 'A';
+        
+        // Untuk menyimpan 2 nilai logika true dan false
+        boolean lulus = true;
+        
+        // Tampilan hasil outputnya
+        System.out.println("Nilai saya: " + nilai);
+        System.out.println("Apakah lulus? " + lulus);
+        
+        /*
+        Perbedaannya yaitu penulisan char menggunakan tanda petik tunggal sedangkan 
+        boolean ditulis langsung. char bisa menampung jenis karaktek dari A-Z, angka, simbol,
+        unicode, dll dan boolean hanya true dan false.
+        */
+        
+    }
+    
+}
