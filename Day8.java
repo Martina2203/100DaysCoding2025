cc68d60f752831c6c3291fa7569d1fb697680b67
Martina2203 <martinatina482@gmail.com>
2025-09-15 17:29:10 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,28 @@
+package day8;
+
+public class Day8 {
+
+    public static void main(String[] args) {
+      
+        // string merupakan tipe data non primitif
+        
+        String nama = "Martina";
+        String alamat = "Lekopadis";
+        String hobby = "Mendengarkan musik";
+        String asalsekolah = "SMKN 1 Tinambung";
+        String prodi = "Informatika";
+        
+        // Menampilkan hasil output
+        System.out.println("Hai perkenalkan nama saya " + nama);
+        System.out.println("Saya berasal dari " + alamat);
+        System.out.println("Saya suka " + hobby);
+        System.out.println("Saya alumni dari " + asalsekolah);
+        System.out.println("Saya kuliah di unsulbar prodi " + prodi);
+        
+        /*
+        String menggunakan tanda kutip ganda (" "), berfugsi untuk menyimpan teks 
+        dan juga bisa menampung banyak karakter sekaligus
+        */
+    }
+    
+}
