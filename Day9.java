e2d43ae031612d1a9cd34afcaa7f70b3cc3850a5
Martina2203 <martinatina482@gmail.com>
2025-09-16 20:12:08 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,33 @@
+package day9;
+
+public class Day9 {
+
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        String nama; 
+        int umur; 
+        
+        // inisialisasi variabel
+        nama = "Martina";
+        umur = 18;
+        
+        // Tampilan nilai awal
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur + " tahun");
+        
+        // Update variable
+        nama = "Tina";
+        umur = 19;
+        
+        // Tampilan nilai setelah update
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur + " tahun");
+        
+        /*
+        Deklarasi membuat variabel dengan tipe data tertentu
+        Inisialisasi memberikan nilai pertama pada variabel
+        Update mengubah nilai lama variabel dengan nilai baru
+        */
+    }
+    
+}
