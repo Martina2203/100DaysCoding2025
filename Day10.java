5cdc4439b852147459416ae76de591b2b86f252d
Martina2203 <martinatina482@gmail.com>
2025-09-17 12:24:50 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,18 @@
+package day10;
+
+public class Day10 {
+
+    public static void main(String[] args) {
+      
+        final float bb  = 35.0f;
+        
+        // Tampilan hasil output
+        System.out.println("Berat badan saya: " + bb + "kg");
+                
+        /* 
+        Final variabel adalah nilai variabel yang tidak bisa diubah setelah diinisialisasi 
+        dan isa digunakan tipe data apapun.
+        */        
+    }
+    
+}
