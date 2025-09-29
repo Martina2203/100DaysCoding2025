b0c29911b8fec18ef5f6007b94ebcfb004590e00
Martina2203 <martinatina482@gmail.com>
2025-09-29 21:19:28 +0800

    Day22.java

---
+++ Day22.java
@@ -0,0 +1,19 @@
+package day22;
+
+import java.util.Scanner;
+
+public class Day22 {
+
+    public static void main(String[] args) {
+        // Program luas persegi
+         Scanner ip = new Scanner(System.in);
+         
+         System.out.print("Masukkan sisi persegi: ");
+         int sisi = ip.nextInt();
+         
+         int luas = sisi * sisi;
+         System.out.println("Luas persegi adalah " + luas);
+         
+    }
+    
+}
