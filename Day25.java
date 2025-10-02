9c768bd3384c4c7981e3b1a6780fc0b3a144f028
Martina2203 <martinatina482@gmail.com>
2025-10-02 20:47:37 +0700

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,23 @@
+package day25;
+
+public class Day25 {
+
+    public static void main(String[] args) {
+        // Operator Increment dan Descrement (++,--)
+        int a1, a2, b1, b2;
+        a1 = 6;
+        a2 = 8;
+        b1 = 5;
+        b2 = 7;
+
+        System.out.println("Post Increment: " + a1++); 
+
+        System.out.println("Pre Increment: " + ++a2); 
+
+        System.out.println("Post Descrement: " + b1--); 
+
+        System.out.println("Pre Descrement: " + --b2); 
+
+    }
+    
+}
