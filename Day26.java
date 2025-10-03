ba336a57ee6d71c8d3ccab9ba005e4cbe42b85a3
Martina2203 <martinatina482@gmail.com>
2025-10-03 18:33:49 +0700

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,27 @@
+package day26;
+
+public class Day26 {
+
+    public static void main(String[] args) {
+        // Operator penugasan (=,+=,-=,dll)
+        int a = 10;
+        System.out.println("Nilai 1 = " + a);
+        
+        a += 5;
+        System.out.println("Nilai 2 = " + a);
+        
+        a -= 8;
+        System.out.println("Nilai 3 = " + a);
+        
+        a *= 5;
+        System.out.println("Nilai 4 = " + a);
+        
+        a /= 5;
+        System.out.println("Nilai 5 = " + a); 
+        
+        a %= 6;
+        System.out.println("Nilai 6 = " + a);
+        
+    }
+    
+}
