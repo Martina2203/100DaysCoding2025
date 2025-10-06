44a36de03d66e7dd4206dc22896370453fff3017
Martina2203 <martinatina482@gmail.com>
2025-10-06 18:34:44 +0700

    Day29.java

---
+++ Day29.java
@@ -0,0 +1,18 @@
+package day29;
+
+import java.util.Scanner;
+
+public class Day29 {
+
+    public static void main(String[] args) {
+        // Operator Perbandingan < dan >
+        Scanner ip = new Scanner(System.in);
+        System.out.print("Masukkan angka 1: ");
+        int a = ip.nextInt();
+        System.out.print("Masukkan angka 2: ");
+        int b = ip.nextInt();
+        System.out.println(a < b);
+        System.out.println(a > b);
+    }
+    
+}
