b4ee6deaaf549234d8527aec021b7ca2b4dbd293
Martina2203 <martinatina482@gmail.com>
2025-10-14 20:26:12 +0700

    Update Day37.java

--- Day37.java
+++ Day37.java
@@ -8,13 +8,15 @@ public class Dav37 {
         // Menentukan bilangan ganjil atau genap
         Scanner p = new Scanner(System.in);
         System.out.print("Masukkan angka: ");
-        int b = p.nextInt();
-        for (int a = 1; a<=b; a++){
-            if(a %2 == 0){
-                System.out.println("Genap\t: " + a);
-            } else 
-                System.out.println("Ganjil\t: " + a);
-        }
+        long b = p.nextLong();
+        
+            if(b %2 == 1 && b < 1000){
+                System.out.printf( "%d adalah bilangan ganjil dan termasuk ratusan \n" , b);
+            } else if (b % 2 == 0 && b < 1000){
+                System.out.println(b + " adalah bilangan genap dan termasuk ratusan ");
+            } else {
+                System.out.println(b + " input tidak valid ");
+            }
+                
+        } 
     }
-    
-}
