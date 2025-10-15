3da3342895cf63473f373ed379e1cb7dcb2f8678
Martina2203 <martinatina482@gmail.com>
2025-10-15 20:07:23 +0700

    Day38.java

--- Day38.java
+++ Day38.java
@@ -9,13 +9,13 @@ public class Day38 {
         Scanner p = new Scanner(System.in);
         System.out.println("==== Menentukan Jenis Bilangan ====\n");
         System.out.print("Masukkan angka: ");
-        int a = p.nextInt();
+        long a = p.nextLong();
         if (a > 0){
-        System.out.println(a + " adalah bilangan positif");
+        System.out.printf("%d adalah bilangan positif%n", a);
         }else if (a < 0){
-        System.out.println(a + " adalah bilangan negatif");
+        System.out.printf("%d adalah bilangan negatif%n", a);
         }else {
-         System.out.println(a + " adalah bilangan nol");   
+         System.out.printf("%d adalah bilangan nol");   
        }
   
     }
