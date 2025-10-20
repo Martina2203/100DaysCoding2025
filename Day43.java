2eb55acae51850e1b432c57f24c5ff988273995b
Martina2203 <martinatina482@gmail.com>
2025-10-20 23:16:43 +0800

    Day43.java

--- Day43.java
+++ Day43.java
@@ -5,43 +5,45 @@ import java.util.Scanner;
 public class Day43 {
     
     public static void main(String[] args) {
-        // Latihan program kelipatan 3,5,3 dan 5
         Scanner q = new Scanner(System.in);
         System.out.print("Masukkan Angka: ");
         int a = q.nextInt();
         
         String jenis = "";
-        if (a < 1){
-        jenis = " tidak boleh memasukkan angka negatif";
-        }else if (a < 10) {
-        jenis = " satuan";
-        }else if (a < 100){
-        jenis = " puluhan";
-        } else if (a >= 100 && a < 1000){
-        jenis = " ratusan";
-        } else{
-        jenis = " tidak valid";
+        if (a < 10 && a >= 1) {
+            jenis = " satuan";
+        } else if (a < 100) {
+            jenis = " puluhan";
+        } else if (a < 1000) {
+            jenis = " ratusan";
+        } else {
+            jenis = " tidak valid";
         }
         
         String bilangan = "";
-        if (a % 2 == 0){
-        bilangan = " genap";
-        }else{
-        bilangan = " ganjil";    
+        if (a % 2 == 0) {
+            bilangan = " genap";
+        } else {
+            bilangan = " ganjil";    
         }
         
         String bagi = "";
-        if (a % 3 == 0 && a % 5 == 0){
-        bagi = " yang bisa di bagi 3 dan 5";
-        }else if (a % 3 == 0){
-        bagi = " yang bisa di bagi 3";
-        }else if (a % 5 == 0){
-        bagi = " yang bisa di bagi 5";
-        }else{
-        bagi = " yang tidak bisa di bagi 3 dan 5";
+        if (a % 3 == 0 && a % 5 == 0) {
+            bagi = " yang bisa dibagi 3 dan 5";
+        } else if (a % 3 == 0) {
+            bagi = " yang bisa dibagi 3";
+        } else if (a % 5 == 0) {
+            bagi = " yang bisa dibagi 5";
+        } else {
+            bagi = " yang tidak bisa dibagi 3 dan 5";
         }
         
-        System.out.println(a + " adalah angka" + jenis + bagi);
+        if (a <= 0) {
+            System.out.println("Tidak bisa memasukkan angka negatif dan nol");
+        } else if (a >= 1000) {
+            System.out.println("Angka tidak valid (terlalu besar)");
+        } else {
+            System.out.println(a + " adalah angka" + jenis + bilangan + bagi);
+        }
     }
-    
 }
