23f60ba3631531d2fd4cb0d519a18d3fec2eae6d
Martina2203 <martinatina482@gmail.com>
2025-10-28 19:18:09 +0700

    Day51.java

--- Day51.java
+++ Day51.java
@@ -7,10 +7,13 @@ public class Day51 {
     public static void main(String[] args) {
         // Perulangan for
         Scanner q = new Scanner(System.in);
-        System.out.print("Masukkan jumlah kucing: ");
-        int j = q.nextInt();
-        for (int k = 1; k <= j; k++){
-            System.out.println("Kucing ke-" + k);
+        System.out.print("Masukkan angka negatif: ");
+        int n = q.nextInt();
+        System.out.print("Masukkan angka positif: ");
+        int p= q.nextInt();
+        for (int a = n; a <= p; a++){
+        String h = (a < 0)? " Negatif" : (a > 0) ? " Positif" : "Nol";
+        System.out.println(a + " adalah bilangan " + h);
         }
     }
     
