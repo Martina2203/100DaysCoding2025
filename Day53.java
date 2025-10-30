7caca0a6f2846453747bdc43774f11f9cc37eecf
Martina2203 <martinatina482@gmail.com>
2025-10-30 18:39:23 +0700

    Day53.java

--- Day53.java
+++ Day53.java
@@ -7,29 +7,22 @@ public class Day53 {
     public static void main(String[] args) {
         // Keyword break menghentikan perulangan
         Scanner q = new Scanner(System.in);
-
         System.out.print("Masukkan jumlah hari: ");
         int j = q.nextInt();
-
         System.out.print("Masukkan target tabungan (Rp): ");
         int t = q.nextInt();
-
         int h = 1;
         int total = 0;
-
         while (h <= j) {
             System.out.println("Hari ke-" + h + " menabung Rp1000");
             total += 1000;
 
-            if (total >= t) { // perbaikan di sini
+            if (total >= t) { 
                 System.out.println("Tabungan mencapai Rp" + total + " di hari ke-" + h);
-                break; // menghentikan perulangan
+                break; 
             }
-
             h++;
         }
-
         System.out.println("Total tabungan Rp " + total);
-        q.close();
     }
 }
