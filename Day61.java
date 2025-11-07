0400e40912c6821c56f07a87bc7b88fab3bb4d32
Martina2203 <martinatina482@gmail.com>
2025-11-07 19:13:54 +0800

    Day61.java

--- Day61.java
+++ Day61.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Day61 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner q = new Scanner(System.in);
         System.out.print("Masukkan nilai N: ");
-        int n = input.nextInt();
+        int n = q.nextInt();
         System.out.print("Masukkan nilai M: ");
-        int m = input.nextInt();
+        int m = q.nextInt();
         System.out.println("\nBilangan kelipatan " + m + " dari 1- " + n + ":");
         for (int i = 1; i <= n; i++) {
             if (i % m == 0) {
