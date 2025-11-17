84fe63a87a51b37c3c6b69f82a2d0a170535e27e
Martina2203 <martinatina482@gmail.com>
2025-11-17 19:29:34 +0800

    Day71.java

---
+++ Day71.java
@@ -0,0 +1,19 @@
+package Day71;
+
+import java.util.Scanner;
+
+public class Day71 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah baris: ");
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {      
+            for (int j = 1; j <= i; j++) {  
+                System.out.print("*");
+            }
+            System.out.println();          
+        }
+    }
+}
