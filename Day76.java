d409301e3903a15b5f34b13627636763d13d86c0
Martina2203 <martinatina482@gmail.com>
2025-11-22 21:14:45 +0800

    Day76.java

--- Day76.java
+++ Day76.java
@@ -14,30 +14,30 @@ public class Day76 {
 
        // equals
         if (kata.equals(pembanding)) {
-            System.out.println("equals(): Kedua kata SAMA persis.");
+            System.out.println("equals(): Kedua kata sama persis.");
         } else {
-            System.out.println("equals(): Kedua kata TIDAK sama persis.");
+            System.out.println("equals(): Kedua kata tidak sama persis.");
         }
 
         // equalsIgnoreCase()
         if (kata.equalsIgnoreCase(pembanding)) {
-            System.out.println("equalsIgnoreCase(): Kedua kata sama jika tidak melihat huruf besar/kecil.");
+            System.out.println("equalsIgnoreCase(): Kedua kata sama meskipun huruf besar atau kecil.");
         } else {
-            System.out.println("equalsIgnoreCase(): Kata berbeda meskipun huruf besar/kecil diabaikan.");
+            System.out.println("equalsIgnoreCase(): Kata berbeda maka diabaikan.");
         }
 
         // contains()
         if (kata.contains(pembanding)) {
-            System.out.println("contains(): Kata pertama MENGANDUNG kata pembanding.");
+            System.out.println("contains(): Kata pertama ada pembanding.");
         } else {
-            System.out.println("contains(): Kata pertama TIDAK mengandung kata pembanding.");
+            System.out.println("contains(): Kata pertama tidak ada kata pembanding.");
         }
 
         // isEmpty()
         if (kata.isEmpty()) {
-            System.out.println("isEmpty(): Kata pertama KOSONG.");
+            System.out.println("isEmpty(): Kata pertama kosong.");
         } else {
-            System.out.println("isEmpty(): Kata pertama TIDAK kosong.");
+            System.out.println("isEmpty(): Kata pertama tidak kosong.");
         }
     }
 }
