be4e477206d058608203b6fa5fdcf6480472346f
Martina2203 <martinatina482@gmail.com>
2025-12-01 19:46:57 +0800

    Day86.java

--- Day86.java
+++ Day86.java
@@ -7,9 +7,15 @@ public class Day86 {
         Scanner q = new Scanner(System.in);
         int n = q.nextInt();
         int[] a = new int[n];
-        for (int i = 0; i < n; i++) a[i] = q.nextInt();
+        for (int i = 0; i < n; i++) {
+            a[i] = q.nextInt();
+        }
         int min = a[0];
-        for (int i = 1; i < n; i++) if (a[i] < min) min = a[i];
+        for (int i = 1; i < n; i++) {
+            if (a[i] < min) {
+                min = a[i];
+            }
+        }
         System.out.println(min);
     }
 }
