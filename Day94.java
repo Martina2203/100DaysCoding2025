4521467ea4bec23142e5b7a484f57d07edb0e833
Martina2203 <martinatina482@gmail.com>
2025-12-01 19:31:33 +0800

    Day94.java

---
+++ Day94.java
@@ -0,0 +1,16 @@
+package Day94;
+
+import java.util.Scanner;
+
+public class Day94 {
+
+    public static void main(String args[]) {
+        Scanner q = new Scanner(System.in);
+        String teks = q.nextLine();
+        cetak(teks);
+    }
+
+    public static void cetak(String t) {
+        System.out.println(t);
+    }
+}
