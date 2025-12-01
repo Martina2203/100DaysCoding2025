d23e68395fc6ee03142ee630695be1919f63d270
Martina2203 <martinatina482@gmail.com>
2025-12-01 19:25:12 +0800

    Day89.java

---
+++ Day89.java
@@ -0,0 +1,13 @@
+package Day89;
+
+public class Day89 {
+
+    public static void main(String args[]) {
+        bintang();
+        bintang();
+    }
+
+    public static void bintang() {
+        System.out.println("*****");
+    }
+}
