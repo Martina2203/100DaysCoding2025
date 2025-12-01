de5458d14a6543e024dbc6ae20be121396a04649
Martina2203 <martinatina482@gmail.com>
2025-12-01 19:27:42 +0800

    Day91.java

---
+++ Day91.java
@@ -0,0 +1,16 @@
+package Day91;
+
+import java.util.Scanner;
+
+public class Day91 {
+
+    public static void main(String args[]) {
+        Scanner q = new Scanner(System.in);
+        String teks = q.nextLine();
+        tampil(teks);
+    }
+
+    public static void tampil(String t) {
+        System.out.println(t);
+    }
+}
