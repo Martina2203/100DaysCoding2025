d4627236cc2cd9d4bc0e9dc595edbdd84504aa37
Martina2203 <martinatina482@gmail.com>
2025-12-03 20:27:31 +0800

    Day87.java

--- Day87.java
+++ Day87.java
@@ -5,10 +5,13 @@ import java.util.Scanner;
 public class Day87 {
     public static void main(String[] args) {
         Scanner q = new Scanner(System.in);
+
         int n = q.nextInt();
         int[] a = new int[n];
 
-        for (int i = 0; i < n; i++) a[i] = q.nextInt();
+        for (int i = 0; i < n; i++) {
+            a[i] = q.nextInt();
+        }
 
         int cari = q.nextInt();
         boolean ketemu = false;
