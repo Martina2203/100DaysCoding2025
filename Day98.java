6ddd866b953ded32adc36794696d9ac6426322a0
Martina2203 <martinatina482@gmail.com>
2025-12-12 19:15:18 +0800

    Day98.java

---
+++ Day98.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Day98 {
+
+    public static void main(String[] args) {
+        Scanner q = new Scanner(System.in);
+
+        int n = q.nextInt();
+
+        int a = 0, b = 1;
+
+        for(int i = 1; i <= n; i++){
+            System.out.print(b + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
