60886f077880df7efd3a9444e02d5c2bd64a8105
Martina2203 <martinatina482@gmail.com>
2025-12-12 19:13:39 +0800

    Day97.java

---
+++ Day97.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Day97 {
+
+    static int luasPersegi(int sisi){
+        return sisi * sisi;
+    }
+
+    public static void main(String[] args) {
+        Scanner q = new Scanner(System.in);
+
+        int s = q.nextInt();
+
+        System.out.println("Luas : " + luasPersegi(s));
+    }
+}
