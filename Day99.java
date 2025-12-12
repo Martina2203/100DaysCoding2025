b5411b229744abe3e8282067007246348c615e02
Martina2203 <martinatina482@gmail.com>
2025-12-12 19:16:40 +0800

    Day99.java

---
+++ Day99.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Day99 {
+
+    static boolean prima(int x){
+        for(int i = 2; i < x; i++){
+            if(x % i == 0) return false;
+        }
+        return x > 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner q = new Scanner(System.in);
+
+        int n = q.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            if(prima(i)){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
