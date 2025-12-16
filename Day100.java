a3e6ddad35839bc2f98743702f899fe78a843c93
Martina2203 <martinatina482@gmail.com>
2025-12-16 22:31:23 +0800

    Day100.java

---
+++ Day100.java
@@ -0,0 +1,33 @@
+package Day100;
+
+import java.util.Scanner;
+
+public class Day100 {
+    public static void main(String[] args) {
+        Scanner q = new Scanner(System.in);
+        String s = q.nextLine();
+
+        boolean adaI = false;
+        boolean adaS = false;
+        boolean adaC = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == 'I' && !adaI) {
+                adaI = true;
+            } else if (ch == 'S' && adaI && !adaS) {
+                adaS = true;
+            } else if (ch == 'C' && adaI && adaS) {
+                adaC = true;
+                break;
+            }
+        }
+
+        if (adaI && adaS && adaC) {
+            System.out.println("CANTIK");
+        } else {
+            System.out.println("TIDAK CANTIK");
+        }
+    }
+}
